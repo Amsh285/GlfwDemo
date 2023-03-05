7d1d448d87e3e9867827a6a57f21c4f9c9ebf846
Dorian Bach <bachdorian@yahoo.de>
2023-03-05 19:09:12 +0100

    initial commit

---
+++ WindowClassTest/WindowClassTest.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "ShaderProgram.h"
+#include "SpdLoggerFactory.h"
+#include "VaoAggregate.h"
+
+
+int main()
+{
+
+
+	return EXIT_SUCCESS;
+}
